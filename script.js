2b508902376aab2b7d6c9a03897da4c87193a0f3
mohanraj <mraj8867@gmail.com>
2023-08-28 20:31:53 +0530

    Refactor game logic and update win condition check.
    This commit refactors the game logic by introducing separate
    functions for toggling player symbols and updating the field and
    player, and also updates the win condition check for player 1 and
    player 2.

--- script.js
+++ script.js
@@ -2,10 +2,62 @@ const gameBoard = (() => {
     const fieldArray = new Array(9);
     const currentPlayer = {};
 
-    return { fieldArray, currentPlayer };
+    const togglePlayerSymbolAndCurrentPlayer = (event) => {
+        if (event.textContent === 'O') {
+            player_1.symbol = 'X';
+            player_2.symbol = 'O';
+            gameBoard.currentPlayer = player_1;
+        } else {
+            player_2.symbol = 'X';
+            player_1.symbol = 'O';
+            gameBoard.currentPlayer = player_2;
+        }
+    }
+
+    const updateFieldAndPlayer = (event) => {
+        const fieldEle = event.target;
+        if (fieldEle.textContent === '' && gameBoard.currentPlayer === player_1) {
+            fieldEle.textContent = player_1.symbol;
+            const fieldIndex = fieldEle.getAttribute('data-index');
+            gameBoard.fieldArray[parseInt(fieldIndex)] = player_1.symbol;
+            gameBoard.currentPlayer = player_2;
+        } else if (fieldEle.textContent === '' && gameBoard.currentPlayer === player_2) {
+            fieldEle.textContent = player_2.symbol;
+            const fieldIndex = fieldEle.getAttribute('data-index');
+            gameBoard.fieldArray[parseInt(fieldIndex)] = player_2.symbol;
+            gameBoard.currentPlayer = player_1;
+        }
+    }
+
+    const checkWinCondition = () => {
+        if ((fieldArray[0] === 'X' && fieldArray[1] === 'X' && fieldArray[2] === 'X')
+            || (fieldArray[3] === 'X' && fieldArray[4] === 'X' && fieldArray[5] === 'X')
+            || (fieldArray[6] === 'X' && fieldArray[7] === 'X' && fieldArray[8] === 'X')
+            || (fieldArray[0] === 'X' && fieldArray[3] === 'X' && fieldArray[6] === 'X')
+            || (fieldArray[1] === 'X' && fieldArray[4] === 'X' && fieldArray[7] === 'X')
+            || (fieldArray[2] === 'X' && fieldArray[5] === 'X' && fieldArray[8] === 'X')
+            || (fieldArray[0] === 'X' && fieldArray[4] === 'X' && fieldArray[8] === 'X')
+            || (fieldArray[2] === 'X' && fieldArray[4] === 'X' && fieldArray[6] === 'X')) {
+            console.log('player - 1 won');
+        } else if ((fieldArray[0] === 'O' && fieldArray[1] === 'O' && fieldArray[2] === 'O')
+            || (fieldArray[3] === 'O' && fieldArray[4] === 'O' && fieldArray[5] === 'O')
+            || (fieldArray[6] === 'O' && fieldArray[7] === 'O' && fieldArray[8] === 'O')
+            || (fieldArray[0] === 'O' && fieldArray[3] === 'O' && fieldArray[6] === 'O')
+            || (fieldArray[1] === 'O' && fieldArray[4] === 'O' && fieldArray[7] === 'O')
+            || (fieldArray[2] === 'O' && fieldArray[5] === 'O' && fieldArray[8] === 'O')
+            || (fieldArray[0] === 'O' && fieldArray[4] === 'O' && fieldArray[8] === 'O')
+            || (fieldArray[2] === 'O' && fieldArray[4] === 'O' && fieldArray[6] === 'O')) {
+            console.log('player - 2 won');
+        } else {
+            console.log('nothing checks out')
+        }
+    }
+
+    return { fieldArray, togglePlayerSymbolAndCurrentPlayer, updateFieldAndPlayer, checkWinCondition };
 })();
 
 const Player = (name, symbol) => {
+
     return { name, symbol };
 };
 
@@ -19,38 +71,14 @@ const symbolList = document.querySelectorAll('.symbols');
 symbolList.forEach(symbolEle => {
 
     symbolEle.addEventListener('click', (event) => {
-        console.log('symbol choosen')
-        console.log(gameBoard.fieldArray.length);
-        if (event.textContent === 'O') {
-            player_1.symbol = 'X';
-            player_2.symbol = 'O';
-            gameBoard.currentPlayer = player_1;
-        } else {
-            player_2.symbol = 'X';
-            player_1.symbol = 'O';
-            gameBoard.currentPlayer = player_2;
-        }
-    }
-    )
+        gameBoard.togglePlayerSymbolAndCurrentPlayer(event);
+    })
 });
 
 const fieldList = document.querySelectorAll('.field');
 fieldList.forEach(field => {
     field.addEventListener('click', (event) => {
-        console.log('field clicked');
-        console.log(event.target);
-        console.log(field);
-        if (field.textContent === '' && gameBoard.currentPlayer === player_1) {
-            field.textContent = player_1.symbol;
-            const fieldIndex = field.getAttribute('data-index');
-            gameBoard.fieldArray[parseInt(fieldIndex)] = player_1.symbol;
-            gameBoard.currentPlayer = player_2;
-        } else if (field.textContent === '' && gameBoard.currentPlayer === player_2) {
-            field.textContent = player_2.symbol;
-            const fieldIndex = field.getAttribute('data-index');
-            gameBoard.fieldArray[parseInt(fieldIndex)] = player_2.symbol;
-            gameBoard.currentPlayer = player_1;
-        }
-        console.log(gameBoard.fieldArray);
+        gameBoard.updateFieldAndPlayer(event);
+        gameBoard.checkWinCondition();
     })
 });
